d46d2af73cca326e07453ef8a16b073cf1018e2e
joseniquen08 <j.armando0807@gmail.com>
2022-11-30 22:11:14 -0500

    fix: THARSIS-5 mail content

--- server/api/src/mailing/mailServices.ts
+++ server/api/src/mailing/mailServices.ts
@@ -3,11 +3,17 @@ import { mailConfig } from '../config/mailConfig';
 
 export const sendMailService = async (email: string) => {
   try {
+    const code = (Math.random() * 100000).toFixed(0);
     const mssg = {
-      from: `${process.env.NODEMAILER_EMAIL}`,
+      from: `Desarrollador de Tharsis - Tharsis Dev <${process.env.NODEMAILER_EMAIL}>`,
       to: email,
-      subject: 'Correo de prueba',
-      text: 'Envío de correo desde tharsis.pe usando nodemailer',
+      subject: `Confirmación de administrador #${code}`,
+      html: `
+        <p>Tu correo ${email} ha sido añadido a la base de datos de Tharsis.</p>
+        <br/>
+        <br/>
+        <p><code>No responder a este correo.</code></p>
+      `,
     }
     const transport = nodemailer.createTransport(mailConfig);
     const info = await transport.sendMail(mssg);
