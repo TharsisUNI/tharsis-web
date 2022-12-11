8d4752d73f9967cacf2eae5ad101b589c8083baa
joseniquen08 <j.armando0807@gmail.com>
2022-12-11 09:45:14 -0500

    feat: THARSIS-6 add navbar and header in landing

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,9 +1,13 @@
+import { About } from '../components/Landing/About';
+import { Header } from "../components/Landing/Header";
 import { Navbar } from "../components/Landing/Navbar";
 
 const Landing = () => {
   return (
     <div className="w-full font-ubuntu">
       <Navbar />
+      <Header />
+      <About />
     </div>
   )
 }
